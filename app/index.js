397f8b6b3fd1bb4b97bfd2dabf9e794ccd563fe2
leorenis <leorenis@gmail.com>
2020-04-14 21:19:37 -0300

    Adjusts file

--- app/index.js
+++ app/index.js
@@ -24,11 +24,11 @@ const histogram = new client.Histogram({
     buckets: [0.1, 0.2, 0.3, 0.4, 0.5],
   });
 
-  const summary = new client.Summary({
-      name: 'class_request_time_seconds_summary',
-      help: 'Summary help',
-      percentiles: [0.5, 0.9, 0.99],
-  })
+const summary = new client.Summary({
+    name: 'class_request_time_seconds_summary',
+    help: 'Summary help',
+    percentiles: [0.5, 0.9, 0.99],
+})
 
 app.get('/', (req, res) => {
     counter.labels('200').inc()
